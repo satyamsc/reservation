931fe4c2d5cee29d9845edf47f01f6b0e680c1d0
satyav <satyamsc@gmail.com>
2025-02-02 15:22:33 +0100

    Terraform changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,117 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "eu-central-1"
 }
 
-resource "aws_lambda_function" "example_lambda" {
-  filename         = "target/your_lambda_function.zip" # Path to your zip file
-  function_name    = "example_lambda_function"
-  role             = "arn:aws:iam::YOUR_ACCOUNT_ID:role/abc" # Replace YOUR_ACCOUNT_ID with your AWS account ID
-  handler          = "com.example.Handler::handleRequest" # Replace with your handler
-  runtime          = "java17"
-  source_code_hash = filebase64sha256("target/your_lambda_function.zip")
+# IAM Role for Lambda (unchanged)
+resource "aws_iam_role" "lambda_role" {
+  name = "park_reservation_lambda-dynamodb-access-role"
 
-  environment {
-    variables = {
-      EXAMPLE_ENV_VAR = "example_value"
-    }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "dynamodb_full" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "eventbridge_full" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess"
+}
+resource "aws_iam_role_policy_attachment" "lambda_full" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSLambda_FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "sns_execution" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSNSFullAccess"
+}
+
+# Lambda Function (unchanged)
+resource "aws_lambda_function" "park_lambda" {
+  filename      = "../target/reservation-1.0-SNAPSHOT-lambda-package.zip"
+  function_name = "reservation-lambda"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "com.parkhere.reservation.StreamLambdaHandler::handleRequest"
+  runtime       = "java17"
+  timeout       = 10
+}
+
+# API Gateway (updated)
+resource "aws_api_gateway_rest_api" "parkhere_api" {
+  name        = "parkhere"
+  description = "ParkHere API Gateway"
+  endpoint_configuration {
+    types = ["REGIONAL"]
   }
 }
 
-resource "aws_iam_role_policy_attachment" "attach_lambda_policy" {
-  role       = "abc"
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.parkhere_api.id
+  parent_id   = aws_api_gateway_rest_api.parkhere_api.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.parkhere_api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.parkhere_api.id
+  resource_id             = aws_api_gateway_resource.proxy.id
+  http_method             = aws_api_gateway_method.proxy.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.park_lambda.invoke_arn
+}
+
+# Lambda Permission (unchanged)
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.park_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.parkhere_api.execution_arn}/*/*"
+}
+
+# Updated API Gateway Deployment and Stage
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.parkhere_api.id
+
+  triggers = {
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.proxy.id,
+      aws_api_gateway_method.proxy.id,
+      aws_api_gateway_integration.lambda_integration.id
+    ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "dev_stage" {
+  stage_name    = "dev"
+  rest_api_id   = aws_api_gateway_rest_api.parkhere_api.id
+  deployment_id = aws_api_gateway_deployment.deployment.id
+}
+
+output "api_url" {
+  value = aws_api_gateway_stage.dev_stage.invoke_url
 }
